5e7247640b8e8d8136ec79b908fda639390f6779
Lipsa23456 <152254809+Lipsa23456@users.noreply.github.com>
2023-11-28 07:30:28 +0000

    javascript Basic done

---
+++ Basic/Text.js
@@ -0,0 +1,8 @@
+console.log("hello world")
+/* 1. Javascript is a high level scripting language used to perform the task on frontend as well as backend.
+   2.Javascript use both client side as well as server side.
+   3.Javascript is most used programming lang.for web development.
+   4.In javascript we use "Esb" version is the latest one developed in 2015.
+   *Esb= Echma script6(vanilla javascript)
+   5.javascript is basically used for designing the logic part of our web pages.
+*/
